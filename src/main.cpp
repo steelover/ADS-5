b3aa438ac76fa52aaa68d3429d3dba26c1a3af7f
steelover <150594943+steelover@users.noreply.github.com>
2024-04-20 19:07:03 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,5 @@
 // Copyright 2021 NNTU-CS
 
 int main() {
-    static int get_priority(char element) {
-        if (element == '(') {
-            return 0;
-        } else if (element == ')') {
-            return 1;
-        } else if ((element == '+') || (element == '-')) {
-            return 2;
-        } else if ((element == '*') || (element == '/')) {
-            return 3;
-        } else {
-            throw "Unknown Symbol!";
-        }
-    }
     return 0;
 }
